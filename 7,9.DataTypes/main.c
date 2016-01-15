50196d21427487cace60051387cd9a7d92ea9356
Julianne <juliannebagsh@gmail.com>
2016-01-15 04:51:17 +0000

    Initial Commit

---
+++ 7,9.DataTypes/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  7,9.DataTypes
+//
+//  Created by Julianne on 15/01/2016.
+//  Copyright (c) 2016 Julianne. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
